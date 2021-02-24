a9838ddc5b56c657af7c82dde4ca9d7c00b73363
Nathan Barlow <nathanjbarlow@gmail.com>
2021-02-24 07:50:25 -0600

    Complete lesson 6

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
-// This is a comment
-
 using namespace std;
 
 int main() {
-    string name;
-    string age;
-    cin >> name;
-    cin >> age;
-    cout << "Hi my name is " << name <<" and I am " << age << " years old";
+    bool isGameOver;
+    isGameOver = false;
+    isGameOver = true;
+    bool isNotGameOver = isGameOver;
+    isGameOver = 5 > 2;
+
+    int currentHealth = 100;
+    currentHealth = 50 - 2;
+
+    float percentHealth = 0.45;
+    double pi = 3.14159;
 }
